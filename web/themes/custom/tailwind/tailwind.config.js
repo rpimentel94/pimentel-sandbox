470d44d38bf9f90b65191a1812814f8d532ad24f
rpimentel94 <ryan.pimentel94@gmail.com>
2023-07-13 11:27:35 -0700

    Setting up tailwind locally & SCSS

--- web/themes/custom/tailwind/tailwind.config.js
+++ web/themes/custom/tailwind/tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+      'postcss-import': {}
+    },
+  ],
   content: [
     'templates/**/*.twig',
     'tailwind.theme',
-    'components/**/*.twig'
+    'components/styles.scss',
+    'components/**/*.twig',
+    'components/**/*.scss',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  mode: 'jit',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 }
