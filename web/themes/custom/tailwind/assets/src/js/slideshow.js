3caa6f2981d9737303063248b3a1d4e7bf50edfb
rpimentel94 <ryan.pimentel94@gmail.com>
2023-09-08 07:43:53 -0700

    Creating change for Drupal Once

--- web/themes/custom/tailwind/assets/src/js/slideshow.js
+++ web/themes/custom/tailwind/assets/src/js/slideshow.js
@@ -1,29 +1,31 @@
 (function (Drupal, once) {
   Drupal.behaviors.swiper = {
     attach(context) {
-      const swiper = new Swiper(".swiper", {
-        // Optional parameters
-        direction: "vertical",
-        loop: true,
 
-        // If we need pagination
-        pagination: {
-          el: ".swiper-pagination",
-        },
-
-        // Navigation arrows
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-          el: ".swiper-scrollbar",
-        },
+      $(once('swiper', 'banner-slideshow', context))
+      .each(function () {
+        const swiper = new Swiper(".swiper", {
+          // Optional parameters
+          direction: "vertical",
+          loop: true,
+  
+          // If we need pagination
+          pagination: {
+            el: ".swiper-pagination",
+          },
+  
+          // Navigation arrows
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+  
+          // And if we need scrollbar
+          scrollbar: {
+            el: ".swiper-scrollbar",
+          },
+        });
       });
-
-      console.log("Firing");
     },
   };
 
