818e0f3a974ab9d0e5f8f4fc353d313fd5efca0a
rpimentel94 <ryan.pimentel94@gmail.com>
2023-09-19 14:44:57 -0700

    Creating JS triggers for other accordion options

---
+++ web/themes/custom/tailwind/assets/src/js/v-tabs.js
@@ -0,0 +1,45 @@
+(function (Drupal, once) {
+  Drupal.behaviors.vTabs = {
+    attach: function (context, settings) {
+
+      let tabsContainer = document.querySelector("#v-tabs");
+
+      let tabTogglers = tabsContainer.querySelectorAll("#v-tabs a");
+
+      tabTogglers.forEach(function (toggler) {
+        toggler.addEventListener("click", function (e) {
+          e.preventDefault();
+
+          let tabName = this.getAttribute("href");
+
+          let tabContents = document.querySelector("#v-tab-contents");
+
+          for (let i = 0; i < tabContents.children.length; i++) {
+            tabTogglers[i].parentElement.classList.remove(
+              "border-t",
+              "border-r",
+              "border-l",
+              "-mb-px",
+              "bg-white"
+            );
+            tabContents.children[i].classList.remove("hidden");
+            if ("#" + tabContents.children[i].id === tabName) {
+              continue;
+            }
+            tabContents.children[i].classList.add("hidden");
+          }
+          e.target.parentElement.classList.add(
+            "border-t",
+            "border-r",
+            "border-l",
+            "-mb-px",
+            "bg-white"
+          );
+        });
+      });
+    },
+  };
+
+  // The parameters are reversed in the callback between jQuery `.each` method
+  // and the native `.forEach` array method.
+})(Drupal, once);
