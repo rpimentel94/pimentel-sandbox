68bdd55e549c1cae0085e77670809ccb329a4a5e
rpimentel94 <ryan.pimentel94@gmail.com>
2023-12-13 09:27:52 -0800

    Adding mobile-ness for tables

---
+++ web/themes/custom/tailwind/assets/src/js/global.js
@@ -0,0 +1,19 @@
+(function (Drupal, drupalSettings, once) {
+    "use strict";
+  
+    Drupal.behaviors.globalFuntions = {
+      attach: function (context, settings) {
+        once("table", "article table", context).forEach((el) => {
+            wrap(el);
+        });
+
+        function wrap(el) {
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('tailwind-table');
+            el.parentNode.insertBefore(wrapper, el);
+            wrapper.appendChild(el);
+        }
+      },
+    };
+  })(Drupal, drupalSettings, once);
+  
