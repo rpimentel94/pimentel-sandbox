9a0cc80ddd7ff1cc40efd5ac01db3eee36446ac0
rpimentel94 <ryan.pimentel94@gmail.com>
2023-12-29 13:37:18 -0800

    Correcting social media icon render

--- web/modules/custom/htlf_social_media/src/Plugin/Block/SocialMediaBlock.php
+++ web/modules/custom/htlf_social_media/src/Plugin/Block/SocialMediaBlock.php
@@ -29,6 +29,15 @@ class SocialMediaBlock extends BlockBase
     $site_settings = $config_pages->load('site_settings');
 
 
+    if ($site_settings->get('field_enable_social_media_icons')->getString() == true ) {
+      $accounts['facebook'] = $site_settings->get('field_facebook_url')->getString() ?: NULL;
+      $accounts['instagram'] = $site_settings->get('field_instagram_url')->getString() ?: NULL;
+      $accounts['linkedin'] = $site_settings->get('field_linkedin_url')->getString() ?: NULL;
+      $accounts['pinterest'] = $site_settings->get('field_pinterest_url')->getString() ?: NULL;
+      $accounts['twitter'] = $site_settings->get('field_twitter_url')->getString() ?: NULL;
+      $accounts['yelp'] = $site_settings->get('field_yelp_url')->getString() ?: NULL;
+      $accounts['youtube'] = $site_settings->get('field_youtube_url')->getString() ?: NULL;
+    }
 
     return [
       '#theme' => 'htlf_social_media_block',
